9340efc90f3448cdbb7d2b87a63807cf503ed92d
João Carlos <joaocarlosanjosbrito@gmail.com>
2026-02-24 16:52:50 -0300

    projeto redbubble estagio 2022

---
+++ assets/js/script.js
@@ -0,0 +1,24 @@
+/* SCRIPT PARA O ANIME SCROLL */
+const target = document.querySelectorAll('[data-anime]');
+const animationClass = 'animate';
+
+function animeScroll () {
+    const windowTop = window.pageYOffset + ((window.innerHeight * 3) / 4);  /* Tamanho geral da section, multiplicou por 3 e dividiu por 4 */
+    target.forEach(function(element){
+        if((windowTop)> element.offsetTop){
+            element.classList.add(animationClass);
+        } else{
+            element.classList.remove(animationClass);
+        }
+
+        console.log(element.offsetTop);
+    })
+}
+
+animeScroll();
+
+if(target.length) {
+ window.addEventListener('scroll', function() {
+    animeScroll();
+ })
+}
